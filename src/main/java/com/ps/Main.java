d024be784db60468e79dfec53105e66a4fc510c5
ahmede <ahmed.salama456@gmail.com>
2022-07-22 02:03:08 +0200

    HashMap - leetcode

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,41 +1,155 @@
 package com.ps;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
 
 public class Main {
     private final static Scanner scan = new Scanner(System.in);
 
+    //https://leetcode.com/problems/design-hashmap/
     public static void main(String[] args) {
-        merge(new int[]{0}, 0
-                , new int[]{1}, 1);
+        Thread t1 = new Thread(new Main().new RunnableImpl());
+        t1.start();
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int totalLength = m + n;
-        int x = 0;
-        for (int i = m; i < totalLength; i++) {
-            nums1[i] = nums2[x];
-            ++x;
+    private class RunnableImpl implements Runnable {
+
+        public void run() {
+            MyHashMap myHashMap = new MyHashMap();
+            myHashMap.put(1, 1);
+            System.out.println(myHashMap);
+            myHashMap.put(2, 2);
+            System.out.println(myHashMap);
+            System.out.println(myHashMap.get(1));
+            System.out.println(myHashMap.get(3));
+            myHashMap.put(2, 1);
+            System.out.println(myHashMap);
+            System.out.println(myHashMap.get(2));
+            myHashMap.remove(2);
+            System.out.println(myHashMap);
         }
-        bubbleSort(nums1, totalLength);
-        System.out.println(Arrays.toString(nums1));
     }
 
-    public static void bubbleSort(int[] sort_arr, int len) {
 
-        for (int i = 0; i < len - 1; ++i) {
-            for (int j = 0; j < len - i - 1; ++j) {
-                if (sort_arr[j + 1] < sort_arr[j]) {
-                    int swap = sort_arr[j];
-                    sort_arr[j] = sort_arr[j + 1];
-                    sort_arr[j + 1] = swap;
+    class MyHashMap {
+        LinkedList<Entry>[] listOfEntries;
+        private int size = 0;
+        private int length = 1;
 
+        public MyHashMap() {
+            listOfEntries = new LinkedList[length];
+//            for (int i = 0; i < length; i++) {
+//                listOfEntries[i] = new LinkedList<Entry>();
+//            }
+        }
+
+        public void put(int key, int value) {
+            if (size == length) {
+                length = length << 2;
+                listOfEntries = Arrays.copyOf(listOfEntries, length);
+            }
+            int keyHashedValue = getHashedKey(key);
+            if (listOfEntries[keyHashedValue] == null) {
+                listOfEntries[keyHashedValue] = new LinkedList<>();
+            } else {
+                LinkedList<Entry> listOfEntry = listOfEntries[keyHashedValue];
+                for (int i = 0; i < listOfEntry.size(); i++) {
+                    Entry entry = listOfEntry.get(i);
+                    if (entry.getKey() == key) {
+                        listOfEntry.set(i, new Entry(key, value));
+                        return;
+                    }
                 }
             }
+            listOfEntries[keyHashedValue].add(new Entry(key, value));
+            ++size;
+        }
+
+        public int get(int key) {
+            int keyHashedValue = getHashedKey(key);
+            LinkedList<Entry> listOfEntry = listOfEntries[keyHashedValue];
+            if (listOfEntry == null) return -1;
+            for (Entry entry : listOfEntry) {
+                if (entry.getKey() == key) {
+                    return entry.getValue();
+                }
+            }
+            return -1;
+        }
+
+        public int getHashedKey(int originalKey) {
+            return originalKey % length;
+        }
+
+        public void remove(int key) {
+            int keyHashedValue = getHashedKey(key);
+            LinkedList<Entry> listOfEntry = listOfEntries[keyHashedValue];
+            if (listOfEntry != null) {
+                for (Iterator<Entry> iterator = listOfEntry.iterator(); iterator.hasNext(); ) {
+                    Entry entry = iterator.next();
+                    if (entry.getKey() == key) {
+                        iterator.remove();
+                        break;
+                    }
+                }
+                if (listOfEntry.size() == 0) {
+                    listOfEntries[keyHashedValue] = null;
+                    --size;
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "MyHashMap{" +
+                    "listOfEntries=" + Arrays.toString(listOfEntries) +
+                    '}';
+        }
+    }
+
+    class Entry {
+        private int key;
+        private int value;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Entry entry = (Entry) o;
+            return key == entry.key;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+
+        public Entry(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
         }
     }
 
