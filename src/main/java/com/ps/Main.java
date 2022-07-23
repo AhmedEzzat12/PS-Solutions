37a9444266dc10e55b635ceb2abf7ad0d8a1fad0
ahmede <ahmed.salama456@gmail.com>
2022-07-23 20:45:15 +0200

    146. LRU Cache - leetcode

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     private final static Scanner scan = new Scanner(System.in);
 
-    //https://leetcode.com/problems/insert-delete-getrandom-o1/
+    //https://leetcode.com/problems/lru-cache/
     public static void main(String[] args) {
         Thread t1 = new Thread(new Main().new RunnableImpl());
         t1.start();
@@ -14,39 +14,90 @@ public class Main {
     private class RunnableImpl implements Runnable {
 
         public void run() {
+
+            LRUCache lruCache = new LRUCache(2);
+            lruCache.put(1, 1);
+            lruCache.put(2, 2);
+            lruCache.get(1);
+            lruCache.put(3, 3);
+            lruCache.get(2);
+            lruCache.put(4, 4);
+            lruCache.get(1);
+            lruCache.get(3);
+            lruCache.get(4);
+            System.out.println(lruCache);
         }
     }
 
-    class RandomizedSet {
+    class LRUCache {
 
-        Map<Integer, Integer> nodes;
+        Map<Integer, LinkedNode> map;
+        LinkedNode head;
+        LinkedNode tail;
 
-        public RandomizedSet() {
-            nodes = new HashMap<>();
+        int capacity;
+
+        public LRUCache(int capacity) {
+            map = new HashMap<>();
+            head = new LinkedNode(0, 0);
+            tail = new LinkedNode(0, 0);
+            head.next = tail;
+            tail.prev = head;
+            this.capacity = capacity;
         }
 
-        public boolean insert(int val) {
-            if (nodes.get(val) == null) {
-                nodes.put(val, val);
-                return true;
+        void addToHead(LinkedNode node) {
+            node.next = head.next;
+            node.next.prev = node;
+            node.prev = head;
+            head.next = node;
+        }
+
+        void removeNode(LinkedNode node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        public int get(int key) {
+            if (map.containsKey(key)) {
+                LinkedNode linkedNode = map.get(key);
+                removeNode(linkedNode);
+                addToHead(linkedNode);
+                return map.get(key).value;
+            }
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            if (map.containsKey(key)) {
+                LinkedNode linkedNode = map.get(key);
+                linkedNode.value = value;
+                map.put(key, linkedNode);
+                removeNode(linkedNode);
+                addToHead(linkedNode);
             } else {
-                return false;
+                LinkedNode linkedNode = new LinkedNode(key, value);
+                map.put(key, linkedNode);
+                if (map.size() > this.capacity) {
+                    map.remove(tail.prev.key);
+                    removeNode(tail.prev);
+                }
+                addToHead(linkedNode);
             }
         }
+    }
 
-        public boolean remove(int val) {
-            if (!nodes.containsKey(val)) {
-                return false;
-            } else {
-                nodes.remove(val);
-                return true;
-            }
+    class LinkedNode {
+        int value;
+        int key;
+
+        public LinkedNode(int key, int value) {
+            this.value = value;
+            this.key = key;
         }
 
-        public int getRandom() {
-            List<Integer> integers = nodes.values().stream().toList();
-            return integers.get((int) ((Math.random() * (integers.size() - 0)) + 0));
-        }
+        LinkedNode prev;
+        LinkedNode next;
     }
 
 }
